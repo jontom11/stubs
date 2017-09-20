532a37c3c14604776f7f8ef0ec9328fd2c7b7282
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-09-20 11:21:23 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,2 @@
+//Enter your Stubhub Application Token Below
+let App_Token = '0f5632aa-6a3f-3fb8-b76f-35dc56e1cfa3';
---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+const port = process.env.PORT || 3001;
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var api = require('./routes/api');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/api', api);
+
+  // The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  console.log('app.get wild card activated!')
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+  app.listen(port);
+  console.log(`Password generator listening on ${port}`);
+module.exports = app;
